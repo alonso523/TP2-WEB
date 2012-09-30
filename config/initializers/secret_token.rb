4d178d351c98c83243f6514c85e1127174354715
alonso523 <alonso523@outlook.com>
2012-09-29 21:01:21 -0600

    Archivos de la segunda tarea programada, Ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TP2Alonso::Application.config.secret_token = '08f38a5de1034e665988a6797a7d722b2e1ad8bdf837174f80b4f871b3c893d1f528ff9d9e17f73ad73d6db942d78f94c40ecd98e13ec69872f410834722548c'
